8a1f7c84cb9b1abb9f6a7e55c8ef2e8f657bf840
Muhammad Rizky Millennianno <muhammad.rizky912@ui.ac.id>
2021-07-10 03:16:16 +0700

    Fully functional. No errors, but not tested

--- src/mission_control.cpp
+++ src/mission_control.cpp
@@ -1,29 +1,33 @@
 #include "ros/ros.h"
-#include "rasendriya/plane.h"
+#include "rasendriya/Dropzone.h"
 #include "mavros_msgs/WaypointReached.h"
 #include "mavros_msgs/Waypoint.h"
 #include "mavros_msgs/WaypointPush.h"
 #include "mavros_msgs/CommandCode.h"
+#include "mavros_msgs/Altitude.h"
 #include "std_msgs/Int8.h"
+#include "std_msgs/Float64.h"
+#include "sensor_msgs/NavSatFix.h"
 #include <math.h>
-#include <list>
+#include <vector>
 
-// tuneable variable
-float focal_length = 2.9 * 1e-6; // in milimeters
-float dropping_altitude = 1.5;
-int wp2_reach_counter = 0;
+// REMINDERS: wp_num STARTS FROM 0
 
-float pixel_to_meter = 0.0002645833;
 int waypoint_reached = 0;
 
 void waypoint_reached_callback(const mavros_msgs::WaypointReached& wp_reached){
 	waypoint_reached = wp_reached.wp_seq;
 }
 
-float x_dz, y_dz, cam_angle;
-float X_coord, Y_coord, r_dist;
-float lat_tgt, lon_tgt;
-float gps_alt, gps_long, gps_lat, gps_hdg;
+int x_dz, y_dz;
+float cam_angle;
+float alt, gps_long, gps_lat, gps_hdg;
+int mission_flag = 1;
+
+mavros_msgs::WaypointPush waypoint_push;
+ros::ServiceClient waypoint_push_client;
+
+// CALLBACKS //
 
 void dropzone_target_callback(const rasendriya::Dropzone& dropzone_loc){
 	x_dz = dropzone_loc.x_dropzone;
@@ -32,92 +36,167 @@ void dropzone_target_callback(const rasendriya::Dropzone& dropzone_loc){
 }
 
 void gps_callback(const sensor_msgs::NavSatFix& gps_data){
-	gps_alt = gps_data.altitude;
 	gps_long = gps_data.longitude;
 	gps_lat = gps_data.latitude;
+}
+
+void alt_callback(const mavros_msgs::Altitude& alt_data){
+	alt = alt_data.relative;
 }
 
 void gps_hdg_callback(const std_msgs::Float64& gps_hdg_data){
 	gps_hdg = gps_hdg_data.data;
 }
 
+void mission_flag_callback(const std_msgs::Int8& mis_flag){
+	mission_flag = mis_flag.data;
+}
+
+// MECHANISMS, CALCULATORS, AND METHODS //
+
+void insert_wp(int _wp_num, mavros_msgs::Waypoint _wp){	
+	waypoint_push.request.waypoints.insert(waypoint_push.request.waypoints.begin() + _wp_num, _wp);
+}
+
+void servo_drop_wp(int servo_ch, int wp_drop_num){
+	mavros_msgs::Waypoint wp_drop_servo;
+
+	wp_drop_servo.frame = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
+	wp_drop_servo.command = mavros_msgs::CommandCode::DO_SET_SERVO;
+	wp_drop_servo.is_current = true;
+	wp_drop_servo.autocontinue = true;
+	wp_drop_servo.param1 = servo_ch;
+	wp_drop_servo.param2 = 1100;
+
+	insert_wp(wp_drop_num, wp_drop_servo);
+
+	if(waypoint_push_client.call(waypoint_push)){
+		ROS_INFO("CHANNEL %d PAYLOAD DROPPING WAYPOINT SENT", servo_ch);
+	}
+	else {
+		ROS_INFO("FAILED TO SEND DROPPING WAYPOINT CHANNEL %d PAYLOAD", servo_ch);
+	}
+}
+
+struct targetCoordinate {
+	float longitude, latitude;
+};
+
+struct targetCoordinate calc_drop_coord(){
+	struct targetCoordinate targetCoordinate;
+	float pi = 3.14159;
+	float R_earth = 6378.1*1e3;
+	float pixel_to_meter = 0.0002645833;
+	float focal_length = 2.9 * 1e-6; // in milimeters
+
+	float X_coord = x_dz*pixel_to_meter*alt/focal_length;
+	float Y_coord = y_dz*pixel_to_meter*alt/focal_length;
+	float r_dist = sqrt(pow(X_coord, 2) + pow(Y_coord, 2));
+	targetCoordinate.latitude = (180/pi)*asin(sin(gps_lat)*cos(r_dist/R_earth)+cos(gps_lat)*sin(r_dist/R_earth)*cos(gps_hdg+cam_angle));
+	targetCoordinate.longitude = gps_long + (180/pi)*atan((sin(gps_hdg+cam_angle)*sin(r_dist/R_earth)*cos(gps_lat)/(cos(r_dist/R_earth)-sin(gps_lat)*sin(targetCoordinate.latitude))));
+	return targetCoordinate;
+}
+
+
 int main(int argc, char **argv) {
+	int wp2_reach_counter = 0;
+	int wp4_reach_counter = 0;
+	int hit_count = 0;
+
 	ros::init(argc, argv, "mission_control");
 	ros::NodeHandle nh;
 
-	Plane rasendriya;
-
 	ros::ServiceClient waypoint_push_client = nh.serviceClient<mavros_msgs::WaypointPush>("/mavros/mission/push");
 
+	ros::Subscriber mission_flag_subscriber = nh.subscribe("mission_flag", 1, mission_flag_callback);
+
 	ros::Subscriber waypoint_reached_subscriber = nh.subscribe("/mavros/mission/reached", 1, waypoint_reached_callback);
+	ros::Subscriber dropzone_target_subscriber = nh.subscribe("dropzone_detector", 3, dropzone_target_callback);
+	ros::Subscriber gps_coordinate_subscriber = nh.subscribe("/mavros/global_position/global", 1, gps_callback);
+	ros::Subscriber alt_subscriber = nh.subscribe("/mavros/altitude", 1, alt_callback);
+	ros::Subscriber gps_hdg_subscriber = nh.subscribe("/mavros/global_position/compass_hdg", 1, gps_hdg_callback);
+
+	ros::Publisher vision_flag_publisher = nh.advertise<std_msgs::Int8>("vision_flag", 1);
 
 	ros::Rate rate(30);
 
-	while(ros::ok()){
+	while(ros::ok() && mission_flag != 0){
 		ros::spin();
 
-		mavros_msgs::WaypointPush waypoint_push;
-		mavros_msgs::Waypoint wp_drop;
-		mavros_msgs::Waypoint wp_repeat;
-
-		// turn on vision node when waypoint 3 has reached
+		ROS_INFO("dropzone x coordinate: %d \ndropzone y coordinate: %d \ncoordinate angle: %f", x_dz, y_dz, cam_angle);
+		ROS_INFO("plane longitude: %f \nplane latitude: %f \nplane altitude: %f \nplane heading: %f", gps_long, gps_lat, alt, gps_hdg);
+		// increase counter if wp3 reached
 		if(waypoint_reached == 2){
-			wp2_reach_counter += wp2_reach_counter;
+			++wp2_reach_counter;
+		}
+		else if(waypoint_reached == 4){
+			++wp4_reach_counter;
 		}
 
-		// calculate dropzone coordinate with camera
-		if ((x_dz && y_dz) > 0){
-			// declare constants
-			float pi = 3.14159;
-			float R_earth = 6378.1*1e3;
+		// turn on vision node when wp3 has reached
+		if(waypoint_reached == 2 && wp2_reach_counter == 1){
+			std_msgs::Int8 vision_flag;
+			vision_flag.data = 1;
+			vision_flag_publisher.publish(vision_flag);
+		}
+		else {
+			std_msgs::Int8 vision_flag;
+			vision_flag.data = -1;
+			vision_flag_publisher.publish(vision_flag);
+		}
 
-			X_coord = x_dz*pixel_to_meter*gps_alt/focal_length;
-			Y_coord = y_dz*pixel_to_meter*gps_alt/focal_length;
-			r_dist = sqrt(pow(X_coord, 2) + pow(Y_coord, 2));
-			lat_tgt = (180/pi)*asin(sin(gps_lat)*cos(r_dist/R_earth)+cos(gps_lat)*sin(r_dist/R_earth)*cos(gps_hdg+cam_angle));
-			lon_tgt = gps_long + (180/pi)*atan((sin(gps_hdg+cam_angle)*sin(r_dist/R_earth)*cos(gps_lat)/(cos(r_dist/R_earth)-sin(gps_lat)*sin(lat_tgt))));
+		// dropzone found, confirm by wait for hit_point
+		bool found_condition = (x_dz && y_dz) > (0 || NAN);
+		while(found_condition){
+			++hit_count;
+		}
 
-			// WP 3
-			wp_drop.frame = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
-			wp_drop.command = mavros_msgs::CommandCode::NAV_WAYPOINT;
-			wp_drop.is_current = false;
-			wp_drop.autocontinue = true;
-			wp_drop.param2 = 1;
-			wp_drop.param3 = 0;
-			wp_drop.x_lat = lat_tgt;
-			wp_drop.y_long = lon_tgt;
-			wp_drop.z_alt = dropping_altitude;
-			waypoint_push.request.waypoints.push_back(wp_drop);
-
-			if(waypoint_push_client.call(waypoint_push)){
-				ROS_INFO("WAYPOINT DROP SENT");
-			}
-			else {
-				ROS_INFO("FAILED TO SEND WAYPOINT DROP");
-			}
-
-			// JUMP TO WP 1 AFTER WP 4 WHEN DETECTION SUCESS;
+		// dropzone confirmed
+		if(hit_count >= 3){
+			ROS_INFO("DROPZONE TARGET AQUIRED. PROCEED TO EXECUTE DROPPING SEQUENCE");
+			// proceed calculate target coordinate
+			targetCoordinate tgt_coord = calc_drop_coord();
+			
+			// send do jump command to WP 1 from WP 4 after dropzone has found as WP 5
+			mavros_msgs::Waypoint wp_repeat;
 			wp_repeat.frame = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
 			wp_repeat.command = mavros_msgs::CommandCode::DO_JUMP;
 			wp_repeat.is_current = false;
 			wp_repeat.autocontinue = true;
 			wp_repeat.param1 = 1;
 			wp_repeat.param2 = 2;
-			waypoint_push.request.waypoints.push_back(wp_repeat);
+			insert_wp(5, wp_repeat);
 
-			if(waypoint_push_client.call(waypoint_push)){
-				ROS_INFO("REPEAT TO WAYPOINT 1");
+			if(waypoint_push_client.call(waypoint_push) && waypoint_push.response.success){
+				ROS_INFO("DO JUMP TO WAYPOINT 1 SUCCESS");
 			}
 			else {
-				ROS_INFO("FAILED TO REPEAT TO WAYPOINT 1");
+				ROS_INFO("FAILED TO REPEAT MISSION");
 			}
-		}
 
-		if(wp2_reach_counter == 1) {
-			rasendriya.servo_drop_wp(7);
-		}
-		else if(wp2_reach_counter == 2) {
-			rasendriya.servo_drop_wp(8);
+
+			// send plane attitude waypoint for dropping as WP 3 in WP 2
+			mavros_msgs::Waypoint wp_nav_drop;
+			wp_nav_drop.frame = mavros_msgs::Waypoint::FRAME_GLOBAL_REL_ALT;
+			wp_nav_drop.command = mavros_msgs::CommandCode::NAV_WAYPOINT;
+			wp_nav_drop.is_current = false;
+			wp_nav_drop.autocontinue = true;
+			wp_nav_drop.param2 = 1;
+			wp_nav_drop.param3 = 0;
+			wp_nav_drop.x_lat = tgt_coord.latitude;
+			wp_nav_drop.y_long = tgt_coord.longitude;
+			wp_nav_drop.z_alt = 1.5; // dropping altitude. please tune
+			insert_wp(3, wp_nav_drop);
+
+			if(waypoint_push_client.call(waypoint_push) && waypoint_push.response.success){
+				ROS_INFO("WAYPOINT NAVIGATION DROP SENT");
+			}
+			else {
+				ROS_INFO("FAILED TO SEND WAYPOINT NAVIGATION DROP");
+			}
+
+			servo_drop_wp(7,4);
+			servo_drop_wp(7,5);
 		}
 
 		rate.sleep();
--- src/vision_programs/vision_dropzone.py
+++ src/vision_programs/vision_dropzone.py
@@ -5,30 +5,44 @@ import numpy as np
 import time
 import math
 import imutils
-from imutils.video import VideoStream
 import argparse
+from imutils.video import VideoStream
+from std_msgs.msg import Int8
 
 # camera resolution width and height parameters
 width = 640
 height = 480
 
+def shutdown():
+    print("Vision program shutting down")
+
+def vision_flag_callback(vis_flag):
+    vision_flag = vis_flag.data
+
 def dropzone_detect():
-    # initialize ros node
-    rospy.init_node('vision_dropzone')
-
-    # initialize ros publisher
-    pub = rospy.Publisher('dropzone_detector', Dropzone, queue_size=10)
-    rate = rospy.Rate(30)
-    msg = Dropzone()
-
     # parsing arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("integers", metavar='N', type=int, help='Video source address')
     args = parser.parse_args(rospy.myargv()[1:])
+
     if args.integers >= 0:
         cam = VideoStream(src=args.integers).start()
     elif args.integers == -1:
         cam = VideoStream(usePiCamera=False).start()
+
+    # initialize ros node
+    rospy.init_node('vision_dropzone')
+
+    # initialize ros publisher
+    pub = rospy.Publisher('dropzone_detector', Dropzone, queue_size=3)
+    rate = rospy.Rate(30)
+    msg = Dropzone()
+    
+    # initialize ros subscriber
+    rospy.Subscriber("vision_flag", Int8, vision_flag_callback)
+    
+    # spin
+    rospy.spin()
 
     time.sleep(2.)
 
@@ -36,7 +50,8 @@ def dropzone_detect():
     lower = np.array([170, 127, 117], dtype='uint8')
     upper = np.array([179, 255, 255],  dtype='uint8')
 
-    while not rospy.is_shutdown():
+    while not (rospy.is_shutdown() and vision_flag == -1):
+        
         # pre process
         img = cam.read()
         img_disp = img.copy()
@@ -78,11 +93,12 @@ def dropzone_detect():
                 cX = i[0] - width/2
                 cY = height/2 - i[1]
                 cAngle = math.degrees(math.atan2(cY, cX))
+                # perlu nambah kasus hit_count
                 msg.x_dropzone = cX
                 msg.y_dropzone = cY
                 msg.center_angle = cAngle
 
-        #cv2.imshow("Camera", img)
+        cv2.imshow("Camera", img)
         #cv2.imshow("Mask", frame)
         #cv2.imshow("Canny Edge", canny)
 
