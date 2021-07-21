82e2a1c117d9f99c998f18adf9b9767afa7edf9e
Muhammad Rizky Millennianno <muhammad.rizky912@ui.ac.id>
2021-07-22 01:22:15 +0700

    Add servo testing code for detailed report

---
+++ src/test_servo.cpp
@@ -0,0 +1,54 @@
+#include "ros/ros.h"
+#include "rasendriya/Dropzone.h"
+#include "mavros_msgs/CommandLong.h"
+
+int x_dz, y_dz;
+
+void dropzone_target_callback(const rasendriya::Dropzone& dropzone_loc){
+	x_dz = dropzone_loc.x_dropzone;
+	y_dz = dropzone_loc.y_dropzone;
+}
+
+bool trigger_servo(int servo_num){
+	mavros_msgs::CommandLong do_set_servo;
+	do_set_servo.request.broadcast = true;
+	do_set_servo.request.command = 183;
+	do_set_servo.request.param1 = servo_num;
+	do_set_servo.request.param2 = 1100;
+
+	if (set_servo_client.call(do_set_servo)) {
+		ROS_INFO("SERVO %d IS TRIGGERED", servo_num);
+		return true;
+	}
+	else {
+		ROS_WARN("FAILED TO TRIGGER SERVO %d", servo_num);
+		return false;
+	}
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "test_servo");
+	ros::NodeHandle nh;
+
+	ros::ServiceClient set_servo_client = nh.serviceClient<mavros_msgs::CommandLong>("/mavros/cmd/command", 1);
+
+	ros::Subscriber dropzone_target_subscriber = nh.subscribe("/rasendriya/dropzone", 3, dropzone_target_callback);
+
+	while(ros::ok()) {
+		if((x_dz && y_dz) > 0) {
+			++hit_count;
+		}
+		else {
+			--hit_count;
+		}
+
+		if(hit_count >= 3) {
+			bool payload1_drop = trigger_servo(6);
+			if(payload1_drop) {
+				trigger_servo(7);
+			}
+		}
+		ros::spinOnce();
+	}
+	return 0;
+}
